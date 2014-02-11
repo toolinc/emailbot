4db011984f36d331655f4f76745885b9e36ee0cb
jovanimtzrico@gmail.com <jovanimtzrico@gmail.com>
2014-02-11 14:05:18 -0600

    Fixing Aprovacion Class

--- Emailbot-persistence/src/main/java/com/tool/emailbot/persistence/domain/Aprovacion.java
+++ Emailbot-persistence/src/main/java/com/tool/emailbot/persistence/domain/Aprovacion.java
@@ -1,5 +1,4 @@
 // Copyright 2014 Tool Inc. 
-
 package com.tool.emailbot.persistence.domain;
 
 import static com.google.common.base.Preconditions.checkNotNull;
@@ -27,7 +26,7 @@ import javax.validation.constraints.NotNull;
 
 /**
  * This class represents an Approval of an email request.
- * 
+ *
  * @author Jovani Rico (jovanimtzrico@gmail.com)
  */
 @Entity
@@ -61,60 +60,60 @@ public class Aprovacion extends Entidad {
     private Date aprovadoEn;
 
     @Deprecated
-    public Aprovacion (){
+    public Aprovacion() {
     }
-    
-    private Aprovacion(Builder builder){
+
+    private Aprovacion(Builder builder) {
 	this.id = builder.id;
 	setDirecctor(builder.director);
 	setPeticion(builder.peticion);
 	setAprovado(builder.tipoAprovacion);
 	setAprovadoEn(builder.aprovadoEn);
     }
-    
+
     @Override
     public UUID getId() {
-        return id;
+	return id;
     }
 
     @Override
     public void setId(UUID id) {
-        this.id = id;
+	this.id = id;
     }
 
     public Trabajador getDirecctor() {
-        return direcctor;
+	return direcctor;
     }
 
     public void setDirecctor(Trabajador direcctor) {
-        this.direcctor = direcctor;
+	this.direcctor = checkNotNull(direcctor);
     }
 
     public Peticion getPeticion() {
-        return peticion;
+	return peticion;
     }
 
     public void setPeticion(Peticion peticion) {
-        this.peticion = peticion;
+	this.peticion =checkNotNull(peticion);
     }
 
     public TipoAprovacion getAprovado() {
-        return aprovado;
+	return aprovado;
     }
 
     public void setAprovado(TipoAprovacion aprovado) {
-        this.aprovado = aprovado;
+	this.aprovado = checkNotNull(aprovado);
     }
 
     public Date getAprovadoEn() {
-        return newDate(aprovadoEn);
+	return newDate(aprovadoEn);
     }
 
     public void setAprovadoEn(Date aprovadoEn) {
-        this.aprovadoEn = newDate(aprovadoEn);
+	this.aprovadoEn = newDate(aprovadoEn);
     }
-    
-    public static class Builder implements EntityBuilder<Aprovacion>{
+
+    public static class Builder implements EntityBuilder<Aprovacion> {
 
 	private UUID id;
 	private Trabajador director;
@@ -126,7 +125,7 @@ public class Aprovacion extends Entidad {
 	    this.director = checkNotNull(director);
 	    return this;
 	}
-	
+
 	public Builder setPeticion(Peticion peticion) {
 	    this.peticion = checkNotNull(peticion);
 	    return this;
@@ -141,32 +140,29 @@ public class Aprovacion extends Entidad {
 	    this.aprovadoEn = checkNotNull(aprovadoEn);
 	    return this;
 	}
-	
+
 	/**
-         * Creates a instances of
-         * {@link com.tool.emailbot.persistence.domain.Aprovacion} given the specified
-         * characteristics on the
-         * {@link com.tool.emailbot.persistence.domain.Aprovacion.Builder}.
-         *
-         * @return a new instance {@link com.tool.emailbot.persistence.domain.Aprovacion}.
-         */
-		
+	 * Creates a instances of {@link com.tool.emailbot.persistence.domain.Aprovacion} given the
+	 * specified characteristics on the
+	 * {@link com.tool.emailbot.persistence.domain.Aprovacion.Builder}.
+	 *
+	 * @return a new instance {@link com.tool.emailbot.persistence.domain.Aprovacion}.
+	 */
 	@Override
 	public Aprovacion build() {
 	    id = UUID.randomUUID();
 	    Aprovacion aprovacion = new Aprovacion(this);
 	    return aprovacion;
 	}
-	
+
 	/**
-         * Provides a new builder.
-         *
-         * @return a new instance of
-         * {@link com.tool.emailbot.persistence.domain.Aprovacion.Builder}.
-         */
-        public static Builder newBuilder() {
-            return new Builder();
-        }
-	
+	 * Provides a new builder.
+	 *
+	 * @return a new instance of
+	 * {@link com.tool.emailbot.persistence.domain.Aprovacion.Builder}.
+	 */
+	public static Builder newBuilder() {
+	    return new Builder();
+	}
     }
 }
