85c07eae376626692abc5d6ee21a2446af4a1665
jovanimtzrico@gmail.com <jovanimtzrico@gmail.com>
2014-04-05 14:10:12 -0600

    Trabajador query exception

--- Emailbot-backend/src/main/java/com/tool/emailbot/adapter/jpa/JpaTrabajadorRepository.java
+++ Emailbot-backend/src/main/java/com/tool/emailbot/adapter/jpa/JpaTrabajadorRepository.java
@@ -1,5 +1,4 @@
 // Copyright 2014 Tool Inc.
-
 package com.tool.emailbot.adapter.jpa;
 
 import com.tool.emailbot.common.adapter.jpa.repository.QueryHelper;
@@ -8,6 +7,7 @@ import com.tool.emailbot.domain.model.Trabajador;
 import com.tool.emailbot.domain.repository.TrabajadorRepository;
 
 import javax.inject.Inject;
+import javax.persistence.NoResultException;
 
 /**
  *
@@ -19,30 +19,34 @@ public class JpaTrabajadorRepository implements TrabajadorRepository {
 
     @Inject
     public JpaTrabajadorRepository(Repository<Trabajador> repository) {
-        this.repository = repository;
+	this.repository = repository;
     }
 
     @Override
     public void create(Trabajador trabajador) {
-        repository.create(trabajador);
+	repository.create(trabajador);
     }
 
     @Override
     public Trabajador update(Trabajador trabajador) {
-        return repository.update(trabajador);
+	return repository.update(trabajador);
     }
 
     @Override
     public void delete(Trabajador trabajador) {
-        repository.delete(trabajador);
+	repository.delete(trabajador);
     }
 
     @Override
     public Trabajador findBy(String numeroTrabajador) {
-        Trabajador t;
-        QueryHelper<Trabajador, Trabajador> qh = repository.newQueryHelper();
-        qh.getQuery().where(qh.getBuilder().equal(qh.getRoot().get(numeroTrabajador), numeroTrabajador));
-        t = qh.getSingleResult();
-        return t;
+	Trabajador t = null;
+	QueryHelper<Trabajador, Trabajador> qh = repository.newQueryHelper();
+	try {
+	    qh.getQuery().where(qh.getBuilder().equal(qh.getRoot().get(numeroTrabajador),
+						      numeroTrabajador));
+	    t = qh.getSingleResult();
+	} catch (NoResultException e) {
+	}
+	return t;
     }
 }
