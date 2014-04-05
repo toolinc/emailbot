3ebc858801786f9c85a1929a0e9c31d6139ba0cc
Edgar Martinez Rico <edgar.martinez.rico@gmail.com>
2014-04-05 13:13:10 -0700

    Fixing the Trabajador Repository Exception handling

--- Emailbot-backend/src/main/java/com/tool/emailbot/adapter/jpa/JpaTrabajadorRepository.java
+++ Emailbot-backend/src/main/java/com/tool/emailbot/adapter/jpa/JpaTrabajadorRepository.java
@@ -1,4 +1,5 @@
 // Copyright 2014 Tool Inc.
+
 package com.tool.emailbot.adapter.jpa;
 
 import com.tool.emailbot.common.adapter.jpa.repository.QueryHelper;
@@ -6,47 +7,52 @@ import com.tool.emailbot.common.domain.repository.Repository;
 import com.tool.emailbot.domain.model.Trabajador;
 import com.tool.emailbot.domain.repository.TrabajadorRepository;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.inject.Inject;
 import javax.persistence.NoResultException;
+import javax.persistence.NonUniqueResultException;
 
 /**
- *
  * @author Jovani Rico (jovanimtzrico@gmail.com)
  */
 public class JpaTrabajadorRepository implements TrabajadorRepository {
 
+    private final Logger logger = LoggerFactory.getLogger(getClass());
     private final Repository<Trabajador> repository;
 
     @Inject
     public JpaTrabajadorRepository(Repository<Trabajador> repository) {
-	this.repository = repository;
+        this.repository = repository;
     }
 
     @Override
     public void create(Trabajador trabajador) {
-	repository.create(trabajador);
+        repository.create(trabajador);
     }
 
     @Override
     public Trabajador update(Trabajador trabajador) {
-	return repository.update(trabajador);
+        return repository.update(trabajador);
     }
 
     @Override
     public void delete(Trabajador trabajador) {
-	repository.delete(trabajador);
+        repository.delete(trabajador);
     }
 
     @Override
     public Trabajador findBy(String numeroTrabajador) {
-	Trabajador t = null;
-	QueryHelper<Trabajador, Trabajador> qh = repository.newQueryHelper();
-	try {
-	    qh.getQuery().where(qh.getBuilder().equal(qh.getRoot().get(numeroTrabajador),
-						      numeroTrabajador));
-	    t = qh.getSingleResult();
-	} catch (NoResultException e) {
-	}
-	return t;
+        Trabajador t = null;
+        QueryHelper<Trabajador, Trabajador> qh = repository.newQueryHelper();
+        try {
+            qh.getQuery().where(qh.getBuilder().equal(qh.getRoot().get(numeroTrabajador),
+                    numeroTrabajador));
+            t = qh.getSingleResult();
+        } catch (NoResultException | NonUniqueResultException exc) {
+            logger.warn(exc.getMessage());
+        }
+        return t;
     }
 }
