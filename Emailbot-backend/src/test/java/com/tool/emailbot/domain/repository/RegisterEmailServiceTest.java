f27fa1c83ba7ddf5ed858dbe8b47d82f9650a709
jovanimtzrico@gmail.com <jovanimtzrico@gmail.com>
2014-04-12 12:15:57 -0500

    creating RegisterEmailServiceTest

--- Emailbot-backend/src/test/java/com/tool/emailbot/domain/repository/RegisterEmailServiceTest.java
+++ Emailbot-backend/src/test/java/com/tool/emailbot/domain/repository/RegisterEmailServiceTest.java
@@ -1,9 +1,11 @@
 // Copyright 2014 Tool Inc.
-
 package com.tool.emailbot.domain.repository;
 
-import com.tool.emailbot.adapter.jpa.JpaTrabajadorRepository;
+import com.tool.emailbot.PersistenceTest;
 import com.tool.emailbot.common.domain.repository.Repository;
+import com.tool.emailbot.domain.model.Dependencia;
+import com.tool.emailbot.domain.model.InformacionContacto;
+import com.tool.emailbot.domain.model.Persona;
 import com.tool.emailbot.domain.model.Peticion;
 import com.tool.emailbot.domain.model.Trabajador;
 import com.tool.emailbot.domain.service.RegisterEmailService;
@@ -11,25 +13,51 @@ import com.tool.emailbot.domain.service.RegisterEmailService;
 import org.junit.Test;
 
 import javax.inject.Inject;
+import javax.transaction.UserTransaction;
 
 /**
  * Test for the class {@link com.tool.emailbot.domain.repository}.
  *
  * @author Jovani Rico (jovanimtzrico@gmail.com.tool.emailbot.domain.model.Trabajadorcom)
  */
-public class RegisterEmailServiceTest {
-    private RegisterEmailService service;
+public class RegisterEmailServiceTest extends PersistenceTest {
+
     @Inject
-    private TrabajadorRepository daoTrabajador;
+    private UserTransaction tx;
+    @Inject
+    private TrabajadorRepository dTrabajador;
+    @Inject
+    private Repository<Trabajador> daoTrabajador;
+    @Inject
+    private Repository<Dependencia> daoDependencia;
     @Inject
     private Repository<Peticion> daoPeticion;
-    private final Trabajador.Builder tbuilder = Trabajador.Builder.newBuilder();
-    private final Peticion.Builder pBuilder = Peticion.Builder.newBuilder();
     
-    
+    private final Peticion.Builder builderPeticion = Peticion.Builder.newBuilder();
+    private final Dependencia.Builder buiderDependencia = Dependencia.Builder.newBuilder();
+    private final InformacionContacto.Builder iBuider = InformacionContacto.Builder.newBuilder();
+    private final Persona.Builder buiderPersona = Persona.Builder.newBuilder();
+    private final Trabajador.Builder builderTrabajador = Trabajador.Builder.newBuilder();
+
     @Test
-    public void shouldRegisterEmail(){
+    public void shouldRegisterEmail() throws Exception {
+	Dependencia dependencia = buiderDependencia.setAbreviacion("DGTIC").setNombre(
+		"Direccion General").build();
+	iBuider.setEmail("jovanimtzrico@gmail.com");
+	buiderPersona.setNombre("Jovani").setApellidoMaterno("Rico").
+		setApellidoPaterno("Martinez").setFechaNacimiento(1990, 07, 26).setHomoclave("ohm").
+		setInformacionContacto(iBuider);
+	Trabajador trabajador = builderTrabajador.setDependencia(dependencia).setDirector(true).
+		setNumeroTrabajador("303204614").setPersona(buiderPersona).build();
+	Peticion peticion = builderPeticion.setEmail("jovani@unam.mx").setUsername("jovani").setTrabajador(trabajador).build();
+	RegisterEmailService res = new RegisterEmailService( dTrabajador, daoPeticion);
+	res.registerEmailRequest(peticion);
 	
-	service = new RegisterEmailService(null, null);
+	tx.begin();
+	em.joinTransaction();
+	daoDependencia.create(dependencia);
+	daoTrabajador.create(trabajador);
+	daoPeticion.create(peticion);
+	tx.commit();
     }
 }
