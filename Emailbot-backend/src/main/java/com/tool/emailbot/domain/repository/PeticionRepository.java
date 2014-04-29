4bcf1c2914ef89d59664381f84b5d1a5042afd98
Edgar Martinez Rico <edgar.martinez.rico@gmail.com>
2014-04-29 12:03:04 -0700

    Fixing the register email service Restful service to expose the functionality to the outside world

--- Emailbot-backend/src/main/java/com/tool/emailbot/domain/repository/PeticionRepository.java
+++ Emailbot-backend/src/main/java/com/tool/emailbot/domain/repository/PeticionRepository.java
@@ -2,18 +2,36 @@
 
 package com.tool.emailbot.domain.repository;
 
+import com.tool.emailbot.domain.model.Estatus;
 import com.tool.emailbot.domain.model.Peticion;
 
+import java.util.List;
+
 /**
+ * Specifies the operations of the Request repository.
  *
  * @author Jovani Rico (jovanimtzrico@gmail.com)
  */
 public interface PeticionRepository {
+
+    /**
+     * @param trabajador
+     */
     void create(Peticion trabajador);
 
     Peticion update(Peticion peticion);
 
     void delete(Peticion trabajador);
 
-    public Peticion findBy(String numeroTrabajador);
+    Peticion findBy(String numeroTrabajador);
+
+    /**
+     * Retrieves the desired number of request from the top of the queue from the persistent
+     * storage. This behaves as queue pop operation from the storage.
+     *
+     * @param estatus specifies the status of the request that will be retrieved
+     * @param size    specifies the number of the request that will be retrieved
+     * @return the list of request retrieved
+     */
+    List<Peticion> find(Estatus estatus, int size);
 }
